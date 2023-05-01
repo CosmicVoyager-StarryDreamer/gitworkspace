842e6b255c9135933724eaad363e097ad87ec034
yuan <yuan@gmail.com>
2023-05-01 22:00:10 +0800

    second git commit

--- gittest.py
+++ gittest.py
@@ -1,1 +1,11 @@
-hello world git
+def sum_of_unique(nums):
+    list1 = []
+    for i in set(nums):
+        if nums.count(i) == 1:
+            list1.append(i)
+    return sum(list1)
+
+nums = [1,2,3,2]
+print(sum_of_unique(nums))
+
+
